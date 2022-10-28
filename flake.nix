5ba394c3fa581c5691da0e6e700b0ce951d9af91
Martin Toman <toman.martin@live.com>
2022-10-27 19:57:45 -0700

    Add crane

--- flake.nix
+++ flake.nix
@@ -1,13 +1,26 @@
 {
   inputs = {
     nix.url = github:MODULUSREBUS/nix;
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nix }:
+  outputs = { self, nix, crane }:
     with nix.lib;
-    eachSystem [ system.x86_64-linux ] (system: {
+    eachSystem [ system.x86_64-linux ] (system: let
+      pkgs = nix.packages.${system};
+      craneLib = crane.lib.${system};
+    in {
       devShell = import ./shell.nix {
-        pkgs = nix.packages.${system};
+        inherit pkgs;
+      };
+      packages.default = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource ./.;
+        buildInputs = with pkgs; [
+          pkg-config
+          openssl
+          protobuf
+        ];
+        PROTOC="${pkgs.protobuf}/bin/protoc";
       };
   });
 }
