41e0f219608b94763c1ae3ec125774facf5d5feb
Martin Toman <toman.martin@live.com>
2023-03-17 15:31:38 -0700

    Add bip39 phrase keypair generation/recovery

--- libdata/src/keypair.rs
+++ libdata/src/keypair.rs
@@ -1,10 +1,13 @@
 //! Export [Keypair].
 //! Define utility functions to [generate] and [derive] [Keypair]s.
 
+use anyhow::Result;
 use blake3::derive_key;
 use datacore::SecretKey;
 use rand_chacha::rand_core::SeedableRng;
 use rand_chacha::ChaCha20Rng;
+use bip39_dict::{Entropy, Mnemonics, ENGLISH, seed_from_mnemonics};
+use getrandom::getrandom;
 
 pub use datacore::{generate_keypair as generate, Keypair};
 
@@ -14,4 +17,36 @@ pub fn derive(key: &SecretKey, name: &str) -> Keypair {
     let seed: <ChaCha20Rng as SeedableRng>::Seed = derive_key(name, &key.to_bytes());
     let mut rng = ChaCha20Rng::from_seed(seed);
     Keypair::generate(&mut rng)
+}
+
+/// Generate a new [Keypair] with a BIP39 mnemonic.
+#[must_use]
+pub fn generate_bip39() -> (Keypair, String) {
+    let mut seed: [u8; 32] = Default::default();
+    getrandom(&mut seed).expect("Could not get RNG");
+
+    let entropy = Entropy::<32>::from_slice(&seed).expect("Could not seed entropy");
+
+    let mnemonics = entropy.to_mnemonics::<24, 8>().expect("Could not get mnemonics");
+    let seed: [u8; 32] = seed_from_mnemonics(
+        &ENGLISH, &mnemonics, b"libdata_keypair_generate_bip39", 2048);
+
+    let mut rng = ChaCha20Rng::from_seed(seed);
+    let keypair = Keypair::generate(&mut rng);
+
+    (keypair, mnemonics.to_string(&ENGLISH))
+}
+
+/// Recover a [Keypair] from a BIP39 mnemonic.
+#[must_use]
+pub fn recover_bip39(phrase: &str) -> Result<Keypair> {
+    let mnemonics = Mnemonics::<24>::from_string(&ENGLISH, phrase)?;
+
+    let seed: [u8; 32] = seed_from_mnemonics(
+        &ENGLISH, &mnemonics, b"libdata_keypair_generate_bip39", 2048);
+
+    let mut rng = ChaCha20Rng::from_seed(seed);
+    let keypair = Keypair::generate(&mut rng);
+
+    Ok(keypair)
 }
---
+++ libdata/tests/keypair.rs
@@ -0,0 +1,13 @@
+use quickcheck::quickcheck;
+
+use libdata::keypair;
+
+
+quickcheck! {
+    fn keypair_bip39_generate_recover() -> bool {
+        let (original, phrase) = keypair::generate_bip39();
+        let recovered = keypair::recover_bip39(&phrase).unwrap();
+
+        recovered.to_bytes() == original.to_bytes()
+    }
+}
